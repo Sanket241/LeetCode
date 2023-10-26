226730ec15a1aeedadc72a398769975fff9a34aa
Sanket Sharma <111504270+Sanket241@users.noreply.github.com>
2023-10-26 23:41:19 +0530

    Add files via upload

--- Reverse Linked List.cpp
+++ Reverse Linked List.cpp
@@ -1,19 +1,19 @@
-class Solution {
-public:
-void reverse(ListNode* &head,ListNode* &prev,ListNode* &curr){
-    if(curr == NULL){
-        head = prev;
-        return;
-    }
-    ListNode* forward = curr -> next;
-    reverse(head,curr,forward);
-    curr -> next = prev;
-}
-    ListNode* reverseList(ListNode* head) {
-        ListNode* prev = NULL;
-        ListNode* curr = head;
-        reverse(head,prev,curr);
-        return head;
+// class Solution {
+// public:
+// void reverse(ListNode* &head,ListNode* &prev,ListNode* &curr){
+//     if(curr == NULL){
+//         head = prev;
+//         return;
+//     }
+//     ListNode* forward = curr -> next;
+//     reverse(head,curr,forward);
+//     curr -> next = prev;
+// }
+//     ListNode* reverseList(ListNode* head) {
+//         ListNode* prev = NULL;
+//         ListNode* curr = head;
+//         reverse(head,prev,curr);
+//         return head;
         
-    }
-};
+//     }
+// };
