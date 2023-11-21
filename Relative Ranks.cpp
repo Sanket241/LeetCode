66a3d0168773e06baaeb73d0a5e0423495dfdacd
Sanket Sharma <111504270+Sanket241@users.noreply.github.com>
2023-11-21 23:47:24 +0530

    Add files via upload

---
+++ Relative Ranks.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<string> findRelativeRanks(vector<int>& score) {
+        priority_queue<pair<int,int> >pq;
+        int N = score.size();
+        vector<string> ans(N);
+
+        for(int i=0;i<N;i++){
+            pq.push({score[i],i});
+        }
+        int i=1;
+        while(!pq.empty()){
+            if(i == 1) ans[pq.top().second] = "Gold Medal";
+            if(i == 2) ans[pq.top().second] = "Silver Medal";
+            if(i == 3) ans[pq.top().second] = "Bronze Medal";
+            
+            else if(i > 3) ans[pq.top().second] = to_string(i);
+            i++;
+            pq.pop();
+        }
+return ans;
+    }
+};
